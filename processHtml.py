8b0829392139a6b875eb710204cb257567454a44
Christian Fetzer <fetzer.ch@gmail.com>
2025-08-28 21:48:45 +0200

    local: Fix processHtml
    Backport https://github.com/biologist79/ESPuino/pull/366

--- processHtml.py
+++ processHtml.py
@@ -20,7 +20,14 @@ except ImportError:
 from flask_minify.parsers import Parser
 import json
 
-try:    
+try:
+    import pkg_resources
+except:
+    print("Trying to Install required module: setuptools\nIf this failes, please execute \"pip install setuptools\" manually.")
+    env.Execute("$PYTHONEXE -m pip install setuptools")
+    import pkg_resources
+
+try:
     import pkg_resources
     pkg_resources.require("minify_html==0.15.0")
     import minify_html
